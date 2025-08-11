26b233f4b61f07c7b8ae9dae7833206b7248dde2
Tomáš Zlámal <zlamal@tomaszlamal.com>
2025-08-11 13:40:21 +0200

    Removed test build from tracking

--- test/CMakeFiles/tmuxscript.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for tmuxscript.
-# This may be replaced when dependencies are built.
--- test/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/s/code/projects/tmuxscript
-# Build directory: /home/s/code/projects/tmuxscript/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(BasicFuncTest "/home/s/code/projects/tmuxscript/test/testexec")
-set_tests_properties(BasicFuncTest PROPERTIES  _BACKTRACE_TRIPLES "/home/s/code/projects/tmuxscript/CMakeLists.txt;15;add_test;/home/s/code/projects/tmuxscript/CMakeLists.txt;0;")
-subdirs("src")
